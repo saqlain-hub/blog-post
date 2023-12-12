23a4b9e0200dfe60c25caf34401954d35e32fa88
Saqlain Javed <saqlainsj12@gmail.com>
2023-12-13 01:48:32 +0500

    Blog UI project with react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import NewPost from "./pages/NewPost";
+import PostPage from "./pages/PostPage";
+import About from "./pages/About";
+import Missing from "./pages/Missing";
+import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/post" element={<NewPost />} />
+        <Route path="/post/:id" element={<PostPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
