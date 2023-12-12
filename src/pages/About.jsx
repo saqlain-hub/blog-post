478722bc5054d577b3f553ffe2672c10c2edf762
Saqlain Javed <saqlainsj12@gmail.com>
2023-12-13 02:57:28 +0500

    blog app complete with routing

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <main className="About">
+      <h2>About</h2>
+      <p>This blog app is a project for learning React and React-Router-Dom</p>
+    </main>
+  );
 };
 
 export default About;
