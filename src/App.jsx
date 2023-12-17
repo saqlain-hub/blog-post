c05aa220331ae1adcfc318344895d499310e85e6
Saqlain Javed <saqlainsj12@gmail.com>
2023-12-18 00:40:02 +0500

    blog project almost complete but is giving an error

--- src/App.jsx
+++ src/App.jsx
@@ -3,45 +3,55 @@ import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import NewPost from "./pages/NewPost";
+import EditPost from "./components/EditPost";
 import PostPage from "./pages/PostPage";
 import About from "./pages/About";
 import Missing from "./pages/Missing";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { format } from "date-fns";
+import api from "./api/posts";
+import useWindowSize from "./hooks/useWindowSize";
+// import useAxiosFetch from "./hooks/useAxiosFetch";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "My first post",
-      datetime: "Dec 12, 2023 02:00 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et architecto, cumque magni esse quos minus nostrum tenetur nam eius accusamus similique libero, voluptate quibusdam qui iste, quis impedit optio aut.",
-    },
-    {
-      id: 2,
-      title: "My 2nd post",
-      datetime: "Dec 13, 2023 01:00 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et architecto, cumque magni esse quos minus nostrum tenetur nam eius accusamus similique libero, voluptate quibusdam qui iste, quis impedit optio aut.",
-    },
-    {
-      id: 3,
-      title: "My 3rd post",
-      datetime: "Dec 25, 2023 04:05 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et architecto, cumque magni esse quos minus nostrum tenetur nam eius accusamus similique libero, voluptate quibusdam qui iste, quis impedit optio aut.",
-    },
-    {
-      id: 4,
-      title: "My 4th post",
-      datetime: "Jan 02, 2023 12:00 AM",
-      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et architecto, cumque magni esse quos minus nostrum tenetur nam eius accusamus similique libero, voluptate quibusdam qui iste, quis impedit optio aut.",
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState("");
   const [postBody, setPostBody] = useState("");
+  const [editTitle, setEditTitle] = useState("");
+  const [editBody, setEditBody] = useState("");
   const navigate = useNavigate();
+  const { width } = useWindowSize();
+
+  // const { data, fetchError, isLoading } = useAxiosFetch(
+  //   "http://localhost:3500/posts"
+  // );
+
+  // useEffect(() => {
+  //   setPosts(data);
+  // }, [data]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get("/posts");
+        setPosts(response.data);
+      } catch (err) {
+        // not in the 200 response range
+        if (err.response) {
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Error: ${err.message}`);
+        }
+      }
+    };
+
+    fetchPosts();
+  }, []);
 
   useEffect(() => {
     const filteredResults = posts.filter(
@@ -52,32 +62,67 @@ function App() {
     setSearchResults(filteredResults.reverse());
   }, [posts, search]);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
     const datetime = format(new Date(), "MMMM dd, yyyy pp");
 
     const newPost = { id, title: postTitle, datetime, body: postBody };
+    try {
+      const response = await api.post("/posts", newPost);
 
-    const allPosts = [...posts, newPost];
-    setPosts(allPosts);
-    setPostTitle("");
-    setPostBody("");
-    navigate("/");
+      const allPosts = [...posts, response.data];
+      setPosts(allPosts);
+      setPostTitle("");
+      setPostBody("");
+      navigate("/");
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
   };
 
-  const handleDelete = (id) => {
-    const postsList = posts.filter((post) => post.id !== id);
-    setPosts(postsList);
-    navigate("/");
+  const handleEdit = async (id) => {
+    const datetime = format(new Date(), "MMMM dd, yyyy pp");
+    const updatedPost = { id, title: editTitle, datetime, body: editBody };
+    try {
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(
+        posts.map((post) => (post.id === id ? { ...response.data } : post))
+      );
+      setEditTitle("");
+      setEditBody("");
+      navigate("/");
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await api.delete(`/posts/${id}`);
+      const postsList = posts.filter((post) => post.id !== id);
+      setPosts(postsList);
+      navigate("/");
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
   };
 
   return (
     <div className="App">
-      <Header title="React JS Blog" />
+      <Header title="React JS Blog" width={width} />
       <Nav search={search} setSearch={setSearch} />
       <Routes>
-        <Route path="/" element={<Home posts={searchResults} />} />
+        <Route
+          path="/"
+          element={
+            <Home
+              posts={searchResults}
+              // fetchError={fetchError}
+              // isLoading={isLoading}
+            />
+          }
+        />
         <Route
           path="/post"
           element={
@@ -87,6 +132,19 @@ function App() {
               setPostTitle={setPostTitle}
               postBody={postBody}
               setPostBody={setPostBody}
+            />
+          }
+        />
+        <Route
+          path="/edit/:id"
+          element={
+            <EditPost
+              posts={posts}
+              handleEdit={handleEdit}
+              editTitle={editTitle}
+              setEditTitle={setEditTitle}
+              editBody={editBody}
+              setEditBody={setEditBody}
             />
           }
         />
---
+++ src/components/EditPost.jsx
@@ -0,0 +1,61 @@
+import { useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+
+const EditPost = ({
+  posts,
+  handleEdit,
+  editBody,
+  setEditBody,
+  editTitle,
+  setEditTitle,
+}) => {
+  const { id } = useParams();
+  const post = posts.find((post) => post.id.toString() === id);
+  useEffect(() => {
+    if (post) {
+      setEditTitle(post.title);
+      setEditBody(post.body);
+    }
+  }, [posts, setEditTitle, setEditBody]);
+  return (
+    <main className="NewPost">
+      {editTitle && (
+        <>
+          <h2>Edit Post</h2>
+          <form className="newPostForm" onSubmit={(e) => e.preventDefault()}>
+            <label htmlFor="postTitle">Title:</label>
+            <input
+              type="text"
+              id="postTitle"
+              required
+              value={editTitle}
+              onChange={(e) => setEditTitle(e.target.value)}
+            />
+            <label htmlFor="postBody">Post:</label>
+            <textarea
+              id="postBody"
+              required
+              value={editBody}
+              onChange={(e) => setEditBody(e.target.value)}
+            ></textarea>
+
+            <button onClick={() => handleEdit(post.id)} type="submit">
+              Submit
+            </button>
+          </form>
+        </>
+      )}
+      {!editTitle && (
+        <>
+          <h2>Post Not Found</h2>
+          <p>Well, that's disappointing.</p>
+          <p>
+            <Link to="/">Vist Our Homepage</Link>
+          </p>
+        </>
+      )}
+    </main>
+  );
+};
+
+export default EditPost;
